3c7cf37bea939df418a5579e22067a9702980e7f
Jhonatan <95220392+Naylor-C@users.noreply.github.com>
2025-03-07 11:48:32 -0300

    Update Chat.js

--- Commands/Body/Chat.js
+++ Commands/Body/Chat.js
@@ -1,11 +1,22 @@
+const { SlashCommandBuilder } = require("discord.js");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const AI = new GoogleGenerativeAI("AIzaSyC6nJ91agGlAhsMg0Ad92AzUfdky4YmjN0");
 const model = AI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-module.exports =
+module.exports = { 
+  data: new SlashCommandBuilder()
+    .setName("chat")
+    .setDescription("retorna uma messagem")
+    .addStringOption( string => string
+    .setName("Input")
+    .setDescription("Digite Sua Pergunta")),
 
-
-
-const prompt = "Explain how AI works";
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+  async function execute (interaction) {
+    const I = interaction.options.getString("Input");
+    const result = await model.generateContent(I);
+    const R = result.response.text();
+   
+    await interaction.reply(R);
+    
+  },
+};
