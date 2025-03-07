4cfb82dbab3a5e429216ce62ce718c63c489c0d0
Jhonatan <95220392+Naylor-C@users.noreply.github.com>
2025-03-07 09:09:05 -0300

    Update Chat.js

--- Commands/Body/Chat.js
+++ Commands/Body/Chat.js
@@ -1,1 +1,11 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const AI = new GoogleGenerativeAI("AIzaSyC6nJ91agGlAhsMg0Ad92AzUfdky4YmjN0");
+const model = AI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+module.exports =
+
+
+
+const prompt = "Explain how AI works";
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
