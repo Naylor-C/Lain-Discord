bb161342730734935c826eb22fd688a0f995b674
Jhonatan <95220392+Naylor-C@users.noreply.github.com>
2025-03-08 16:02:13 +0000

    command add Ban

--- Commands/Maneger/Ban.js
+++ Commands/Maneger/Ban.js
@@ -1,1 +1,33 @@
+const { SlashCommandBuilder, }= require("discord.js");
 
+module.exports = {
+   data: new SlashCommandBuilder()
+    .setName("ban")
+    .setDescription("ban user")
+
+    .addUserOption((A) => A
+     .setName("user")
+     .setDescription("select user")
+     .setRequired(true)
+    )
+    .addStringOption((X) => X 
+     .setName("reason")
+     .setDescription("reason of ban")
+     .setRequired(true) 
+    ),
+
+   async execute (interaction) {
+    try {
+      const U = interaction.options.getUser("user");
+      const S = interaction.options.getString("reason");
+
+      await interaction.guild.members.ban(U, { reason: S });
+      await interaction.reply({ content:`{U.tag} foi banido com sucess`, ephemeral: true });
+    } 
+    catch (e) {
+      await interaction.reply({ content:`return:${e}`, ephemeral: true});
+    }
+
+   },
+
+};
