66302b22202dc8c9db4d6d280ff4a8043a1a5567
Jhonatan <95220392+Naylor-C@users.noreply.github.com>
2025-03-12 00:08:48 +0000

    plays

--- Commands/Player/PlayVideo.js
+++ Commands/Player/PlayVideo.js
@@ -1,1 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord,js");
+const { createAudioPlayer } = require('@discordjs/voice');
+const player = createAudioPlayer();
 
+module.exports = {
+   data: new SlashCommandBuilder()
+   .setName("play-video")
+   .setDescription("Retorn is Menu"),
+   
+   async execute (interaction) 
+   {
+    const E = EmbedBuilder()
+    .setColor(0x0099FF)
+
+    await interaction.reply({embed: [E]});
+
+    
+   }
+
+};
